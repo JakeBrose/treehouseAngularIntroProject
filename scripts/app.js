2e0d666a1fc09b8ae33329bc1c0087ad41faefc5
JakeBrose <mordecaistudios@gmail.com>
2018-01-26 15:21:47 -0600

    covered scope

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller('mainCtrl', function ($scope) {
+    $scope.helloWorld = function () {
+        console.log('Hello there');
+    };
+})
+
+.controller('coolCtrl', function ($scope) {
+    $scope.whoAmI = function () {
+        console.log('Hey, bro. This is the cool controller function');
+    }
+    $scope.helloWorld = function () {
+        console.log('tHis iS nOt ThE maIn CoNtrOLler');
+    }
+})   
+
+.controller('imaSibling', function ($scope) {
+    $scope.foobar = 1234;
+});
